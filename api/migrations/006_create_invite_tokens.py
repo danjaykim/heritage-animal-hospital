91831e0548dc058d5cbf224ed993fea847d37ee3
Daniel Kim <danjkim831@gmail.com>
2024-12-04 21:31:47 -0800

    Created an invite_token table

---
+++ api/migrations/006_create_invite_tokens.py
@@ -0,0 +1,19 @@
+steps = [
+    [
+        # Up
+        """--sql
+            CREATE TABLE
+                IF NOT EXISTS invite_tokens (
+                    id SERIAL PRIMARY KEY,
+                    email VARCHAR(100) UNIQUE NOT NULL,
+                    token VARCHAR(100) UNIQUE NOT NULL,
+                    expiration TIMESTAMP NOT NULL
+                    used BOOLEAN DEFAULT FALSE
+                );
+        """,
+        # Down
+        """--sql
+            DROP TABLE IF EXISTS invite_tokens;
+        """,
+    ]
+]
