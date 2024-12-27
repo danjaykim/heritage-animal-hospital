24bb187c1e36e060a6c3dd40e1e5c4d35ca2ab70
Daniel Kim <danjkim831@gmail.com>
2024-12-26 23:34:19 -0800

    Installed framer motion and added dropdown animation on mobile nav menu/links

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,30 +1,63 @@
 import { useState } from 'react'
+import { motion } from 'motion/react'
 import Hamburger from 'hamburger-react'
 import NavLinks from './NavLinks'
 
 export default function Nav() {
     const [isMenuOpen, setIsMenuOpen] = useState(false)
 
-    return (
-        <>
-            <nav className="containers flex justify-between items-center border-2">
-                {/* LOGO */}
-                <p>Heritage of NWA</p>
+    const menuPulldown = {
+        open: {
+            y: 0,
+            transition: {
+                type: 'spring',
+                stiffness: 250,
+                damping: 30,
+            },
+        },
+        close: {
+            y: -400,
+            transition: {
+                type: 'spring',
+                stiffness: 250,
+                damping: 30,
+            },
+        },
+    }
 
-                {/* HAMBURGER ICON */}
-                <div className="lg:hidden">
-                    <Hamburger
-                        toggled={isMenuOpen}
-                        toggle={setIsMenuOpen}
-                        size={25}
-                        rounded
-                        label="Open Hamburger Menu"
-                    />
+    return (
+        <nav className="fixed w-full z-50 bg-white">
+            <div className="relative bg-white">
+                <div className="containers flex items-center justify-between py-4">
+                    {/* LOGO */}
+                    <p>Heritage of NWA</p>
+
+                    {/* HAMBURGER ICON */}
+                    <div className="lg:hidden">
+                        <Hamburger
+                            toggled={isMenuOpen}
+                            toggle={setIsMenuOpen}
+                            direction="right"
+                            size={25}
+                            rounded
+                            label="Open Hamburger Menu"
+                        />
+                    </div>
+
+                    {/* DESKTOP NAV MENU/LINKS */}
+                    <NavLinks className="hidden lg:flex lg:gap-6" />
                 </div>
 
-                {/* DESKTOP NAV MENU/LINKS */}
-                <NavLinks className="hidden lg:flex lg:gap-6" />
-            </nav>
-        </>
+                {/* MOBILE NAV MENU/LINKS OPEN */}
+                <motion.div
+                    initial="close"
+                    animate={isMenuOpen ? 'open' : 'close'}
+                    variants={menuPulldown}
+                    className="absolute -z-10 top-full w-full py-6 bg-neutral-200 lg:hidden"
+                >
+                    <NavLinks className="flex flex-col items-center gap-4" />
+                </motion.div>
+            </div>
+        </nav>
     )
 }
