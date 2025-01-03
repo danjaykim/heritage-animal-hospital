d1fe81b22c3b6333f930c5dbda1005c55a4c5e46
Daniel Kim <danjkim831@gmail.com>
2025-01-03 14:42:56 -0800

    Adjusted code to proper viewing in mobile horizontal mode

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,61 +1,75 @@
 import heroFrenchie from '../assets/images/hero-frenchie.webp'
 import HeroButton from './HeroButton'
 
+import Test from './Test'
+
 export default function Hero() {
     return (
-        <main
-            className="relative overflow-hidden flex flex-col justify-between"
-            style={{ height: `calc(100vh - 65px)` }}
-        >
-            <div className="hero-content w-[90%] lg:max-w-[1400px] left-1/2 transform -translate-x-1/2 absolute flex flex-col gap-6 mt-8 lg:mt-14">
-                <h1 className="font-cal text-3xl md:text-5xl lg:w-[55%] text-[#1A2954]">
-                    Delivering Exceptional Veterinary Care with Compassion,
-                    Integrity, and Commitment{' '}
-                    <span className="hidden lg:inline">
-                        to Grow the Human-Animal Bond
-                    </span>
-                </h1>
-                <p className="hidden md:block md:w-[75%] lg:w-[35%] font-light md:text-[#1A2954] md:text-2xl">
-                    Proudly serving expert care for{' '}
-                    <span className="font-bold">small animals</span> and{' '}
-                    <span className="font-bold">equine</span> throughout
-                    Northwest Arkansas and beyond
-                </p>
-                <HeroButton to="/test">
-                    <div className="flex items-center gap-2">
-                        <span>Book Appointment</span>
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 74 74"
-                            className="w-[34px] transform duration-300 ease-in-out group-hover:translate-x-1.5"
-                        >
-                            <circle
-                                strokeWidth="3"
-                                stroke="#fff"
-                                r="35.5"
-                                cy="37"
-                                cx="37"
-                            ></circle>
-                            <path
-                                fill="#fff"
-                                d="M25 35.5C24.1716 35.5 23.5 36.1716 23.5 37C23.5 37.8284
+        <>
+            <main
+                className="relative flex flex-col justify-between"
+                style={{ minHeight: `calc(100dvh - 65px)` }}
+            >
+                <img
+                    src={heroFrenchie}
+                    alt="French Bulldog Puppy"
+                    className="w-full min-h-screen md:max-h-[900px] object-cover"
+                />
+                <div className="hero-content w-[90%] lg:max-w-[1400px] left-1/2 transform -translate-x-1/2 absolute flex flex-col gap-3 md:gap-6 mt-8 lg:mt-14">
+                    <h1 className="font-cal text-3xl md:text-5xl lg:w-[55%] text-[#1A2954]">
+                        Delivering Exceptional Veterinary Care with Compassion,
+                        Integrity, and Commitment{' '}
+                        <span className="hidden lg:inline">
+                            to Grow the Human-Animal Bond
+                        </span>
+                    </h1>
+                    <p className="hidden md:block md:w-[75%] lg:w-[35%] font-light md:text-[#1A2954] md:text-2xl">
+                        Proudly serving expert care for{' '}
+                        <span className="font-bold">small animals</span> and{' '}
+                        <span className="font-bold">equine</span> throughout
+                        Northwest Arkansas and beyond
+                    </p>
+                    <p className="font-light text-[#1A2954] text-2xl md:hidden">
+                        Call: 123-456-7891
+                    </p>
+                    <HeroButton to="/test">
+                        <div className="flex items-center gap-2">
+                            <span>Book Appointment</span>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 74 74"
+                                className="w-[34px] transform duration-300 ease-in-out group-hover:translate-x-1.5"
+                            >
+                                <circle
+                                    strokeWidth="3"
+                                    stroke="#fff"
+                                    r="35.5"
+                                    cy="37"
+                                    cx="37"
+                                ></circle>
+                                <path
+                                    fill="#fff"
+                                    d="M25 35.5C24.1716 35.5 23.5 36.1716 23.5 37C23.5 37.8284
                                 24.1716 38.5 25 38.5V35.5ZM49.0607 38.0607C49.6464 37.4749
                                 49.6464 36.5251 49.0607 35.9393L39.5147 26.3934C38.9289 25.8076
                                 37.9792 25.8076 37.3934 26.3934C36.8076 26.9792 36.8076 27.9289 
                                 37.3934 28.5147L45.8787 37L37.3934 45.4853C36.8076 46.0711 
                                 36.8076 47.0208 37.3934 47.6066C37.9792 48.1924 38.9289 48.1924
                                 39.5147 47.6066L49.0607 38.0607ZM25 38.5L48 38.5V35.5L25 35.5V38.5Z"
-                            ></path>
-                        </svg>
-                    </div>
-                </HeroButton>
-            </div>
-            <img
-                src={heroFrenchie}
-                alt="French Bulldog Puppy"
-                className="w-full h-full object-cover"
-            />
-        </main>
+                                ></path>
+                            </svg>
+                        </div>
+                    </HeroButton>
+                    <HeroButton
+                        to="/"
+                        className="hidden md:flex md:items-center h-[66px]"
+                    >
+                        Learn about Our Services
+                    </HeroButton>
+                </div>
+            </main>
+            <Test />
+        </>
     )
 }
--- client/src/components/HeroButton.jsx
+++ client/src/components/HeroButton.jsx
@@ -1,12 +1,12 @@
 import { Link } from 'react-router-dom'
 
-export default function HeroButton({ children, to }) {
+export default function HeroButton({ children, to, className }) {
     return (
         <Link
             to={to}
-            className="cursor-pointer group transition-all duration-200 
+            className={`cursor-pointer group transition-all duration-200 
             px-5 py-3 md:px-6 md:py-4 rounded-full font-medium text-lg md:text-xl
-            text-[#fff] bg-[#19535F] backdrop-blur-sm w-fit"
+            text-[#fff] bg-[#19535F] hover:bg-[#1E6C76] w-fit ${className}`}
         >
             {children}
         </Link>
