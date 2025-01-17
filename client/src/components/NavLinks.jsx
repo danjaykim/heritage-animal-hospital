c9c964a56edb51ef12d93a9501958decfbed4d27
Daniel Kim <danjkim831@gmail.com>
2025-01-16 20:37:22 -0800

    Added new isSubDropdownOpen state, dropdown bools, dropdownLinks in navLinks

--- client/src/components/NavLinks.jsx
+++ client/src/components/NavLinks.jsx
@@ -1,11 +1,43 @@
+import { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 
-export default function NavLinks({ className }) {
+export default function NavLinks({ className, isMobile }) {
+    const [isSubDropdownOpen, setIsSubDropdownOpen] = useState(false)
     const navLinks = [
         { title: 'Home', path: '/' },
-        { title: 'About', path: '/' },
-        { title: 'Services', path: '/' },
-        { title: 'Resources', path: '/' },
+        {
+            title: 'About',
+            path: '/about',
+            dropdown: true,
+            dropdownLinks: [
+                { title: 'Our Team', path: '/about#our-team' },
+                { title: 'Our Mission', path: '/about#our-mission' },
+            ],
+        },
+        {
+            title: 'Services',
+            path: '/services',
+            dropdown: true,
+            dropdownLinks: [
+                { title: 'Small Animal', path: '/services#small-animal' },
+                {
+                    title: 'Equine / Live Stock',
+                    path: '/services#equine-livestock',
+                },
+            ],
+        },
+        {
+            title: 'Resources',
+            path: '/resources',
+            dropdown: true,
+            dropdownLinks: [
+                {
+                    title: 'Request Appointment',
+                    path: '/resources/appointment',
+                },
+                { title: 'FAQ', path: '/resources/faq' },
+            ],
+        },
         { title: 'Contact', path: '/' },
     ]
 
