5c8fa69c23ce69370a2e76568d112bb4c61116a7
Daniel Kim <danjkim831@gmail.com>
2025-01-22 23:20:29 -0800

    Rotating services complete (set an interval of 2 seconds to update currServiceIndex state, causing unmount/mount of each service. Utilized AnimatePresence component for smooth exit transition)

--- client/src/components/CareOverview/RotatingServices.jsx
+++ client/src/components/CareOverview/RotatingServices.jsx
@@ -1,16 +1,35 @@
 import { useState, useEffect } from 'react'
+import { motion, AnimatePresence } from 'motion/react'
+import { rotatingServices } from './rotatingServicesData'
 import { rotatingServicesVariants } from '../../animations/animations'
 
 export default function RotatingServices() {
+    const [currServiceIndex, setCurrServiceIndex] = useState(0)
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrServiceIndex((prev) => (prev + 1) % rotatingServices.length)
+        }, 2000)
+        return () => clearInterval(interval)
+    })
+
     return (
-        <section className="care-overview containers flex justify-center items-center my-8 lg:my-12 border-4">
+        <section className="care-overview containers flex justify-center items-center my-8 lg:my-24">
             <div className="flex flex-col items-center text-[2.5rem] lg:text-6xl">
-                <h2 className="text-[#1A2954] font-bold">
-                    We provide care for
-                </h2>
-                <p className="text-[#3F5E98] -mt-4 lg:mt-0">
-                    musculoskeletal issues
-                </p>
+                <h2 className="text-[#1A2954]">We provide care for</h2>
+                <AnimatePresence mode="wait">
+                    <motion.p
+                        key={rotatingServices[currServiceIndex]}
+                        variants={rotatingServicesVariants}
+                        initial="initial"
+                        animate="animate"
+                        exit="exit"
+                        transition={{ duration: 0.5 }}
+                        className="text-[#3F5E98] -mt-4 lg:mt-0"
+                    >
+                        {rotatingServices[currServiceIndex]}
+                    </motion.p>
+                </AnimatePresence>
             </div>
         </section>
     )
