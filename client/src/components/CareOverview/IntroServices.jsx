0a79e0526d0f05d4038e736c0a28496e01f7e571
Daniel Kim <danjkim831@gmail.com>
2025-01-30 17:42:05 -0800

    Tweaked styling on IntroServices for better UI on all viewports

--- client/src/components/CareOverview/IntroServices.jsx
+++ client/src/components/CareOverview/IntroServices.jsx
@@ -8,16 +8,16 @@ import cat from '../../assets/images/cat.webp'
 
 export default function IntroServices() {
     return (
-        <div className="flex flex-col md:flex-row gap-10 text-[#1A2954]">
-            <div className="flex flex-col gap-5 md:w-1/2">
+        <div className="flex flex-col md:flex-row gap-14 text-[#1A2954]">
+            <div className="flex flex-col flex-1 gap-5 lg:items-center md:w-1/2">
                 <div className="flex md:justify-center">
                     <img
                         src={cat}
                         alt="Vet and cat illustration"
-                        className="w-1/2"
+                        className="w-2/5 md:w-1/3"
                     />
                 </div>
-                <p className="font-light leading-6">
+                <p className="flex-1 font-light leading-6 lg:text-center lg:w-3/4">
                     Our animal hospital provides expert care for small animals,
                     including routine exams, vaccinations, dental care, and
                     surgical procedures. We focus on personalized treatment to
@@ -26,7 +26,7 @@ export default function IntroServices() {
                 </p>
                 <Link
                     to="/services#small-animal"
-                    className="w-fit flex items-center gap-1 -mt-2 group"
+                    className="w-fit flex items-center gap-2 group"
                 >
                     <span>Small Animal Services</span>
                     <svg
@@ -34,7 +34,7 @@ export default function IntroServices() {
                         width="12"
                         height="12"
                         viewBox="0 0 24 24"
-                        className="mb-[.05rem] transform duration-300 ease-in-out group-hover:translate-x-1"
+                        className="transform duration-300 ease-in-out group-hover:translate-x-1"
                     >
                         <path
                             d="M7.293 4.707 14.586 12l-7.293 7.293 1.414 1.414L17.414 12 8.707 3.293 7.293 4.707z"
@@ -44,24 +44,23 @@ export default function IntroServices() {
                     </svg>
                 </Link>
             </div>
-            <div className="flex flex-col gap-5 md:w-1/2">
+            <div className="flex flex-col flex-1 gap-5 lg:items-center md:w-1/2">
                 <div className="flex md:justify-center">
                     <img
                         src={horse}
                         alt="Horse and vet illustration"
-                        className="w-1/2"
+                        className="w-2/5 md:w-1/3"
                     />
                 </div>
-                <p className="font-light leading-6">
+                <p className="flex-1 font-light leading-6 lg:text-center lg:w-3/4">
                     We offer specialized care for equine and livestock animals,
                     including emergency treatment, preventative care, and
                     reproductive health management. Our team is equipped to
-                    handle both routine and complex needs, with farm calls
-                    available for on-site services.
+                    handle both routine and complex needs for your animals.
                 </p>
                 <Link
                     to="/services#equine-livestock"
-                    className="w-fit flex items-center gap-1 -mt-2 group"
+                    className="w-fit flex items-center gap-2 group"
                 >
                     <span>Equine and Livestock Services</span>
                     <svg
@@ -69,7 +68,7 @@ export default function IntroServices() {
                         width="12"
                         height="12"
                         viewBox="0 0 24 24"
-                        className="mb-[.05rem] transform duration-300 ease-in-out group-hover:translate-x-1"
+                        className="transform duration-300 ease-in-out group-hover:translate-x-1"
                     >
                         <path
                             d="M7.293 4.707 14.586 12l-7.293 7.293 1.414 1.414L17.414 12 8.707 3.293 7.293 4.707z"
