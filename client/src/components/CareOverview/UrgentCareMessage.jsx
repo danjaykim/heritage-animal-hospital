7025709fbb60ab87da424d2b0bb7d2aefd7d77a4
Daniel Kim <danjkim831@gmail.com>
2025-02-05 19:43:18 -0800

    Absolute positioning on images on mobile and large viewports. Adjustments will be made on medium viewport next

--- client/src/components/CareOverview/UrgentCareMessage.jsx
+++ client/src/components/CareOverview/UrgentCareMessage.jsx
@@ -1,19 +1,31 @@
-// import kittenSleeping from '../../assets/images/alexander-possingham-CeWNEEsHPbA-unsplash.jpg'
 import emCat from '../../assets/images/em-cat.webp'
+import emCat2 from '../../assets/images/kanashi-3dejnak626k-unsplash.jpg'
+import emDog from '../../assets/images/jay-wennington-vXjNwYi2B8M-unsplash.jpg'
 
 export default function UrgentCareMessage() {
     return (
-        <section className="containers flex flex-col lg:flex-row md:gap-10">
-            <img
-                src={emCat}
-                alt="Cat sitting"
-                className="w-full lg:max-w-[500px] 
-                mb-8 md:mb-0 object-cover lg:object-contain
-                max-h-[400px] md:max-h-[300px] lg:max-h-[700px] rounded-xl"
-            />
+        <section className="containers flex flex-col lg:flex-row lg:justify-between md:gap-8">
+            <div className="h-[23rem] lg:w-1/2 relative">
+                <img
+                    src={emDog}
+                    alt="Cat sitting"
+                    className="md:hidden w-1/2 lg:w-1/2
+                    rounded-xl object-cover lg:block
+                    max-h-56 md:max-h-[300px] lg:max-h-[700px]
+                    absolute top-28 right-6 z-10 lg:right-10 lg:top-16"
+                />
+                <img
+                    src={emCat2}
+                    alt="Cat sitting"
+                    className="w-1/2 md:w-full lg:w-1/2
+                    rounded-xl object-cover
+                    max-h-56 md:max-h-[19rem] lg:max-h-[700px]
+                    absolute left-6 md:left-0 lg:top-0"
+                />
+            </div>
             <div className="text-[#1A2954] lg:w-1/2">
-                <p className="font-dm text-4xl mb-4">
-                    Emergency and Urgent Care Walk-ins Welcome
+                <p className="font-dm text-4xl lg:text-6xl mb-4">
+                    Emergency and urgent care walk-ins welcome
                 </p>
                 <p className="font-extralight">
                     We understand that emergencies can happen at any time.
