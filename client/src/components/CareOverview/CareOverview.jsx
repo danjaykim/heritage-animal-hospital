87bebca013d3bffd94edeff653b43cbae5fdc53f
Daniel Kim <danjkim831@gmail.com>
2025-02-14 10:12:45 -0800

    Rearranged IntroMessage above RotatingServices. Also added full width of h2 on large viewports while keeping width a bit smaller on sm/md viewports for IntroMessage

--- client/src/components/CareOverview/CareOverview.jsx
+++ client/src/components/CareOverview/CareOverview.jsx
@@ -6,8 +6,8 @@ import UrgentCareMessage from './UrgentCareMessage'
 export default function CareOverview() {
     return (
         <div className="care-overview flex flex-col gap-20 lg:gap-28 justify-center items-center my-8 lg:my-24">
-            <RotatingServices />
             <IntroMessage />
+            <RotatingServices />
             <IntroServices />
             <UrgentCareMessage />
         </div>
--- client/src/components/CareOverview/IntroMessage.jsx
+++ client/src/components/CareOverview/IntroMessage.jsx
@@ -1,13 +1,24 @@
 export default function IntroMessage() {
     return (
-        <section className="containers flex flex-col items-center gap-5 md:gap-8 text-[#1A2954]">
-            <h2 className="font-dm self-start md:self-center text-[2.3rem] md:text-[2.7rem] lg:text-[3.2rem] leading-none md:leading-[2.5rem] lg:leading-[3rem]">
-                Compassionate care <div>for your beloved pets</div>
+        <section className="containers flex flex-col items-center gap-5 text-[#1A2954]">
+            <h2 className="font-dm self-start md:self-center text-[2.2rem] md:text-[2.7rem] lg:text-[3rem] leading-none md:leading-[2.5rem] lg:leading-[3rem]">
+                Compassionate care
+                <span className="hidden lg:inline">
+                    &nbsp;for your beloved pets
+                </span>
+                <div className="lg:hidden">for your beloved pets</div>
             </h2>
-            <p className="font-light tracking-wide leading-6 md:w-1/2 lg:w-1/3 md:text-center">
+            <p className="font-light tracking-wide leading-6 md:w-3/4 lg:w-full md:text-center">
                 At Heritage Animal Hospital, we treat your pets like family,
-                providing compassionate care and expert treatment in a
-                comfortable, welcoming environment.
+                providing compassionate care and
+                <span className="lg:hidden">
+                    &nbsp;expert treatment in a comfortable, welcoming
+                    environment.
+                </span>
+                <div className="hidden lg:block">
+                    expert treatment in a comfortable, welcoming environment.
+                    See our veterinarians below.
+                </div>
             </p>
         </section>
     )
