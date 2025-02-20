9628af4da2c9bd1bcdff1821042cedf5bf91020f
Daniel Kim <danjkim831@gmail.com>
2025-02-19 21:16:16 -0800

    Revised all p tags to Links to route to their respective destinations (except Contact)

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,8 +1,9 @@
+import { Link } from 'react-router-dom'
 import logo from '../assets/images/heri-logo.webp'
 
 export default function Footer() {
     return (
-        <footer className="bg-[#F5F3EF] text-[#1A2954] font-extralight pt-16 pb-8">
+        <footer className="bg-[#F5F3EF] text-[#1A2954] font-extralight pt-20 pb-10">
             <div className="containers">
                 <div className="flex flex-col md:flex-row md:justify-between gap-14">
                     <img
@@ -12,28 +13,58 @@ export default function Footer() {
                     />
                     <div className="grid grid-rows-2 grid-cols-2 md:grid-cols-4 md:grid-rows-none gap-8 lg:gap-12">
                         <div>
-                            <p className="mb-2">About</p>
+                            <p className="font-semibold mb-2">About</p>
                             <div>
-                                <p>Our Team</p>
-                                <p>Our Mission</p>
+                                <Link
+                                    to="/about#our-team"
+                                    className="w-fit block"
+                                >
+                                    Our Team
+                                </Link>
+                                <Link
+                                    to="/about#our-mission"
+                                    className="w-fit block"
+                                >
+                                    Our Mission
+                                </Link>
                             </div>
                         </div>
                         <div>
-                            <p className="mb-2">Services</p>
+                            <p className="font-semibold mb-2">Services</p>
                             <div>
-                                <p>Small Animal</p>
-                                <p>Equine/Livestock</p>
+                                <Link
+                                    to="/services#small-animal"
+                                    className="w-fit block"
+                                >
+                                    Small Animal
+                                </Link>
+                                <Link
+                                    to="/services#equine-livestock"
+                                    className="w-fit block"
+                                >
+                                    Equine/Livestock
+                                </Link>
                             </div>
                         </div>
                         <div>
-                            <p className="mb-2">Resources</p>
+                            <p className="font-semibold mb-2">Resources</p>
                             <div>
-                                <p>FAQ</p>
-                                <p>Appointments</p>
+                                <Link
+                                    to="resources/faq"
+                                    className="w-fit block"
+                                >
+                                    FAQ
+                                </Link>
+                                <Link
+                                    to="/resources/appointments"
+                                    className="w-fit block"
+                                >
+                                    Appointments
+                                </Link>
                             </div>
                         </div>
                         <div>
-                            <p className="mb-2">Contact</p>
+                            <p className="font-semibold mb-2">Contact</p>
                             <div>
                                 <p>Phone</p>
                                 <p>Email</p>
