e5406a86e528de0a8527a5caf92c71964fb81adc
Daniel Kim <danjkim831@gmail.com>
2025-02-25 19:52:42 -0800

    Mapped nav links in the footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom'
 import logo from '../assets/images/heri-logo.webp'
+import { navLinks } from './Nav/navLinkData'
 
 export default function Footer() {
     return (
@@ -12,57 +13,30 @@ export default function Footer() {
                         className="w-[65px] h-fit md:w-[80px]"
                     />
                     <div className="grid grid-rows-2 grid-cols-2 md:grid-cols-4 md:grid-rows-none gap-8 lg:gap-12">
-                        <div>
-                            <p className="font-semibold mb-2">About</p>
-                            <div>
-                                <Link
-                                    to="/about#our-team"
-                                    className="w-fit block"
-                                >
-                                    Our Team
-                                </Link>
-                                <Link
-                                    to="/about#our-mission"
-                                    className="w-fit block"
-                                >
-                                    Our Mission
-                                </Link>
-                            </div>
-                        </div>
-                        <div>
-                            <p className="font-semibold mb-2">Services</p>
-                            <div>
-                                <Link
-                                    to="/services#small-animal"
-                                    className="w-fit block"
-                                >
-                                    Small Animal
-                                </Link>
-                                <Link
-                                    to="/services#equine-livestock"
-                                    className="w-fit block"
-                                >
-                                    Equine/Livestock
-                                </Link>
-                            </div>
-                        </div>
-                        <div>
-                            <p className="font-semibold mb-2">Resources</p>
-                            <div>
-                                <Link
-                                    to="resources/faq"
-                                    className="w-fit block"
-                                >
-                                    FAQ
-                                </Link>
-                                <Link
-                                    to="/resources/appointments"
-                                    className="w-fit block"
-                                >
-                                    Appointments
-                                </Link>
-                            </div>
-                        </div>
+                        {navLinks
+                            .slice(0, navLinks.length - 1)
+                            .map((link, index) => {
+                                return (
+                                    <div key={index}>
+                                        <p className="font-semibold mb-2">
+                                            {link.title}
+                                        </p>
+                                        {link.dropdownLinks.map(
+                                            (subLink, subIndex) => {
+                                                return (
+                                                    <Link
+                                                        key={subIndex}
+                                                        to={subLink.path}
+                                                        className="w-fit block"
+                                                    >
+                                                        {subLink.title}
+                                                    </Link>
+                                                )
+                                            }
+                                        )}
+                                    </div>
+                                )
+                            })}
                         <div>
                             <p className="font-semibold mb-2">Contact</p>
                             <div>
