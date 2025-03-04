4be9225ccfe1dedcc115365050143055a5248ea9
Daniel Kim <danjkim831@gmail.com>
2025-03-03 19:44:51 -0800

    Testing About component

--- client/src/components/About/About.jsx
+++ client/src/components/About/About.jsx
@@ -1,12 +1,22 @@
 import Mission from './Mission'
 import Team from './Team'
+import aboutHeader from '../../assets/images/about-header.webp'
 
 export default function About() {
     return (
-        <section className="containers">
-            <h1>About Us</h1>
-            <Team />
-            <Mission />
+        <section>
+            <img
+                src={aboutHeader}
+                alt="Dogs playfully running"
+                className="w-full object-cover h-[20vh] md:h-[25vh] lg:h-[30vh]"
+            />
+            <div className="containers my-8">
+                <div className="font-dm text-5xl text-[#1A2954] flex justify-center">
+                    <h1>About Us</h1>
+                </div>
+                <Team />
+                <Mission />
+            </div>
         </section>
     )
 }
--- client/src/components/About/Team.jsx
+++ client/src/components/About/Team.jsx
@@ -1,1 +1,25 @@
-export default function Team() {}
+export default function Team() {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="36"
+            height="36"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="lucide lucide-scan-heart"
+        >
+            <path
+                d="M11.246 16.657a1 1 0 0 0 1.508 0l3.57-4.101A2.75 
+                            2.75 0 1 0 12 9.168a2.75 2.75 0 1 0-4.324 3.388z"
+            />
+            <path d="M17 3h2a2 2 0 0 1 2 2v2" />
+            <path d="M21 17v2a2 2 0 0 1-2 2h-2" />
+            <path d="M3 7V5a2 2 0 0 1 2-2h2" />
+            <path d="M7 21H5a2 2 0 0 1-2-2v-2" />
+        </svg>
+    )
+}
