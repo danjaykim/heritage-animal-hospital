72d2309bf3ee4d54898a91c814f5b1b6d35f19e5
Daniel Kim <danjkim831@gmail.com>
2025-03-19 15:45:51 -0700

    Updated 'read me' title to be absolutely positioned on cat image for a more modern aesthetic

--- client/src/components/About/About.jsx
+++ client/src/components/About/About.jsx
@@ -19,23 +19,28 @@ export default function About() {
 
     return (
         <section>
-            <img
-                src={aboutHeader}
-                alt="Cat lying in the sun"
-                className="w-full object-cover h-[20vh] md:h-[20vh] lg:h-[30vh]"
-            />
-            <div className="containers my-10 md:my-16 lg:my-20 text-[#1A2954]">
-                <div
-                    className="font-nyght font-medium tracking-wider 
-                    text-4xl md:text-5xl flex flex-col justify-center 
-                    items-center"
-                >
-                    <h1>about us</h1>
-                    <div
-                        className="w-[100px] md:w-[140px] h-[2px] 
-                        bg-[#1A2954] mt-2 md:mt-4 lg:mt-6"
-                    ></div>
+            <div className="relative">
+                <img
+                    src={aboutHeader}
+                    alt="Cat lying in the sun"
+                    className="w-full object-cover h-[20vh] lg:h-[30vh]"
+                />
+                <div className="absolute bottom-[-10%] w-full">
+                    <div className="containers flex justify-end">
+                        <h1
+                            className="px-3 pt-[.45rem] pb-[1.15rem] 
+                            md:px-5 md:pt-[.85rem] md:pb-[1.5rem] 
+                            lg:px-8 lg:pt-3 lg:pb-[2rem]             
+                            font-nyght font-bold tracking-wider text-[#1A2954]
+                            text-3xl md:text-[2.5rem] lg:text-5xl
+                            bg-white rounded-t-2xl"
+                        >
+                            about us
+                        </h1>
+                    </div>
                 </div>
+            </div>
+            <div className="containers my-10 md:my-16 lg:my-20 text-[#1A2954]">
                 <Team ref={teamRef} />
                 <Mission ref={missionRef} />
             </div>
