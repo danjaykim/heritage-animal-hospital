2dab36c08847b2c0d42448683f94e448a802e067
Daniel Kim <danjkim831@gmail.com>
2025-03-29 12:06:11 -0700

    Added FAQ data with QA's. Will add onto this shortly

--- client/src/components/Resources/Faq.jsx
+++ client/src/components/Resources/Faq.jsx
@@ -1,3 +1,33 @@
+import {
+    Disclosure,
+    DisclosureButton,
+    DisclosurePanel,
+} from '@headlessui/react'
+
+import chevronDown from '../../assets/images/svg/chevron-down.svg'
+
 export default function Faq() {
-    return <h1>FAQ</h1>
+    return (
+        <section>
+            <h1>FAQ</h1>
+            <Disclosure>
+                <DisclosureButton>Question</DisclosureButton>
+                <DisclosurePanel>Answer</DisclosurePanel>
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    className="bi bi-chevron-down"
+                    viewBox="0 0 16 16"
+                >
+                    <path
+                        d="M1.5 5.5a.5.5 0 0 1 .707-.708L8 
+                        9.293l5.793-5.793a.5.5 0 1 1 .707.708l-6 
+                        6a.5.5 0 0 1-.707 0l-6-6z"
+                    />
+                </svg>
+            </Disclosure>
+        </section>
+    )
 }
---
+++ client/src/components/Resources/FaqData.js
@@ -0,0 +1,136 @@
+export const faqData = [
+    {
+        category: 'general',
+        faqs: [
+            {
+                question: `What is your hospital's your hours of operation?`,
+                answer: 'Our hours of operation are [ ]. We are closed [ ].',
+            },
+            {
+                question: 'Where is Heritage Animal Hospital located?',
+                answer: 'We are located at 1591 W. Bowen, Goshen AR 72703. You can find directions here: [ ].',
+            },
+            {
+                question:
+                    'Do I need to make an appointment, or do you do walk-ins?',
+                answer: `We highly recommend booking an appointment to minimize wait times. 
+                However, we do accept walk-ins for urgent care and emergency services 
+                during regular business hours Monday through Friday.`,
+            },
+            {
+                question: 'What payment methods do you accept?',
+                answer: `We accept cash, debit/credit cards, and pet insurance.
+                Please feel free to reach out to us to confirm if your insurance
+                provider is accepted with us.`,
+            },
+            {
+                question: 'Do you offer emergency services?',
+                answer: 'Yes, we do. If your pet needs urgent care, please reach out to us at 123-123-1234',
+            },
+        ],
+    },
+    {
+        category: 'Services and Treatments',
+        faqs: [
+            {
+                question: 'What types of animals do you service?',
+                answer: `We specialize in treating a wide variety of animals including dogs,
+                cats, horses, cows, pigs, rabbits, hamsters, guinea pigs, ...`,
+            },
+            {
+                question: 'Can I bring in my pet for a routine check-up?',
+                answer: `Absolutely! Routine check-ups help detect potential
+                health issues early and keep your pet in optimal health. We
+                recommend yearly check-ups for your furry pet.`,
+            },
+            {
+                question: 'Do you offer vaccinations for pets?',
+                answer: `Yes! We offer core and non-core vaccinations. Our
+                vets can help determine the best vaccination schedule for your pet.`,
+            },
+            {
+                question: 'Do you offer spay and neutering services?',
+                answer: `Yes, we do perform spay and neutering surgeries. Please
+                feel free to reach out to us to schedule an appointment.`,
+            },
+            {
+                question: 'Do you offer dental care for pets?',
+                answer: `Yes, we offer dental cleanings and extractions to maintain
+                your pet's oral health.`,
+            },
+        ],
+    },
+    {
+        category: 'Appointments and Policies',
+        faqs: [
+            {
+                question: 'How do I schedule an appointment?',
+                answer: `You can schedule an appointment by calling us at
+                    (123)123-1234 or booking online here: [ ]`,
+            },
+            {
+                question: 'What is your cancellation policy?',
+                answer: `We kindly ask that you provide at least [ ] notice
+                if you need to reschedule or cancel your appointment`,
+            },
+            {
+                question: `What should I bring for my pet's first visit?`,
+                answer: `Please bring any previous medical records, a list of
+                medications your pet is taking, and a sample of their current
+                diet if possible.`,
+            },
+        ],
+    },
+    {
+        category: 'Pet Health and Wellness',
+        faqs: [
+            {
+                question: 'What are some signs that my pet might be sick?',
+                answer: `Common signs of illness may be loss of appetite,
+                vomiting, diarrhea, excessive thirst, lethargy, and unusual
+                behavior. If you witness any of these symptoms, please contact
+                us immediately.`,
+            },
+            {
+                question: 'What should I do if my pet has fleas or ticks?',
+                answer: `We offer flea and tick treatments. If your pet has
+                an infestation, schedule an appointment with us, and we'll
+                help you find the optimal treatment for your furry friend!`,
+            },
+            {
+                question: `How can I keep my pet's weight healthy?`,
+                answer: `A balanced diet and regular exercise are crucial to
+                your pet's health. We can provide nutritional counseling to
+                help manange their health.`,
+            },
+            {
+                question: 'How can I tell if my pet is in pain?',
+                answer: `Pet's often hide their pain, but signs to watch out
+                for include limping, decreased appetite, excessive licking
+                of a body part, restlessness, aggression, crying, or sudden
+                changes in behavior. If you notice any of these symptoms,
+                please reach out to us immediately.`,
+            },
+            {
+                question: `How can I keep my pet's teeth healthy?`,
+                answer: `Dental disease is quite common in pets, but regular
+                brushing with a pet-safe toothpaste, dental chews, and regular
+                cleanings can prevent issues like tartar build-up, gum disease,
+                and tooth loss. Signs of dental disease include bad breath, redness
+                or bleeding of gums, and difficulty eating.`,
+            },
+            {
+                question: `What foods are toxic to my pets?`,
+                answer: `Some common foods that are toxic to pets include chooclate,
+                onions, grapes, garlic, xylitol, caffiene, alcohol, and nuts. If your
+                pet ingests any of these, please contact us immediately.`,
+            },
+            {
+                question: `How do I help my senior pet stay active and happy?',
+                answer: 'Senior pets may experience arthritis, decreased energy,
+                and changes in appetite. Providing a healthy diet, orthopedic bed,
+                and regular check-ups can help keep them happy and mobile.`,
+            },
+        ],
+    },
+]
